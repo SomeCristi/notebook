02ac4da8f3a94fda0fe6ef1f7d9b0288962d10cd
Cristian Somesan <crs@flink.to>
2021-04-07 23:52:40 +0300

    Add to render note component in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import NoteForm from './components/Notes/NoteForm'
+
 function App() {
   return (
     <div className="App">
-
+      <NoteForm />
     </div>
   );
 }
