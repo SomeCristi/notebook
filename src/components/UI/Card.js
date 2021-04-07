36a6cb21a701ce0af3fed7607cdbcf2a48ca94d5
Cristian Somesan <crs@flink.to>
2021-04-07 23:53:13 +0300

    Fix mistake in card component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,5 +3,7 @@ import React from 'react';
 import './Card.css'
 
 const Card = props => {
-  return <div className="card">{props.childeren}</div>;
+  return <div className="card">{props.children}</div>;
 }
+
+export default Card;
