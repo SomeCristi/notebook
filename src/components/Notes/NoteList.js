a37aba8c168b36a95900ca1a99b13e7deb53f377
Cristian Somesan <crs@flink.to>
2021-04-12 00:47:29 +0300

    Add key to card list

--- src/components/Notes/NoteList.js
+++ src/components/Notes/NoteList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from "react-bootstrap/Card";
 
@@ -6,7 +7,7 @@ const NoteList = props => {
   return(
     <CardDeck>
       {props.notes.map(note => (
-        <Card style={{ width: '18rem' }}>
+        <Card key={Math.random()} style={{ width: '18rem' }}>
           <Card.Body>
             <Card.Title>{note.title}</Card.Title>
             <Card.Text>{note.body}</Card.Text>
