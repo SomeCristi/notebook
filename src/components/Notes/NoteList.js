c89f93ea9bbcc6c86bb8a10e999a6d8aa3c35eed
Cristian Somesan <crs@flink.to>
2021-04-12 10:18:16 +0300

    Change card width

--- src/components/Notes/NoteList.js
+++ src/components/Notes/NoteList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import CardDeck from 'react-bootstrap/CardDeck';
-import Card from "react-bootstrap/Card";
+import Card from 'react-bootstrap/Card';
 
 const NoteList = props => {
   return(
     <CardDeck>
       {props.notes.map(note => (
-        <Card key={Math.random()} style={{ width: '18rem' }}>
+        <Card key={Math.random()} style={{ minWidth: '30rem' }}>
           <Card.Body>
             <Card.Title>{note.title}</Card.Title>
             <Card.Text>{note.body}</Card.Text>
