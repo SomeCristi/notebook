c0fa6c106f7d0ad4fd4bb41a3516c18576dcc97c
Cristian Somesan <crs@flink.to>
2021-04-16 10:05:17 +0300

    Make the notes a class component + configure to add the newly created note into the firebase databse

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import NoteForm from './NoteForm';
 import NoteList from './NoteList';
+import axios from '../../axios-notes';
 
-const Notes = () => {
-  const [ notes, setNotes ] = useState([]);
-  const addNoteHandler = note => {
-    setNotes(prevNotes => [...prevNotes, note]);
+class Notes extends Component {
+  state = { notes: null };
+
+  componentDidMount() {
+    axios.get('/notes.json')
+      .then(response => {
+        this.setState( { notes: response.data} )
+      })
+      .catch(error => {});
+  }
+  
+  addNoteHandler = note => {
+    axios.post('/notes.json', note);
+    this.setState(prevNotes => ({
+      notes: [...this.state.notes, note]
+    }));
   };
 
-  return (
+  return() {
     <section>
       <div>
-      <NoteForm addNote={addNoteHandler}/>
+      <NoteForm addNote={this.addNoteHandler}/>
       </div>
       <div>
-        <NoteList notes={notes}/>
+        <NoteList notes={this.state.notes}/>
       </div>
     </section>
-  );
+  };
 }
 
 export default Notes;
