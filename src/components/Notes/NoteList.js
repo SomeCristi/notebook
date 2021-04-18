2772a6eb63869b42a9194d0199b052585d10d39e
Cristian Somesan <crs@flink.to>
2021-04-19 01:02:42 +0300

    Add to show message if there are no notes

--- src/components/Notes/NoteList.js
+++ src/components/Notes/NoteList.js
@@ -4,19 +4,25 @@ import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 
 const NoteList = props => {
-  return(
-    <CardDeck>
-      {props.notes.map(note => (
-        <Card key={Math.random()} style={{ minWidth: '30rem', maxWidth: '50rem', marginBottom: '10px' }}>
-          <Card.Body>
-            <Card.Subtitle className="mb-4 text-muted">{note.category}</Card.Subtitle>
-            <Card.Title style={{ fontSize: '24px' }}>{note.title}</Card.Title>
-            <Card.Text>{note.body}</Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
-    </CardDeck>
-  );
+  console.log(props.notes);
+  if(props.notes) {
+    return(
+      <CardDeck>
+        {props.notes.map(note => (
+          <Card key={Math.random()} style={{ minWidth: '30rem', maxWidth: '50rem', marginBottom: '10px' }}>
+            <Card.Body>
+              <Card.Subtitle className="mb-4 text-muted">{note.category}</Card.Subtitle>
+              <Card.Title style={{ fontSize: '24px' }}>{note.title}</Card.Title>
+              <Card.Text>{note.body}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </CardDeck>
+    );
+  }
+  else {
+    return (<p>You have no notes</p>);
+  }
 };
 
 export default NoteList;
