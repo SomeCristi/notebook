40270d3bd46c6c17caac497cb5db66344a2624e2
Cristian Somesan <crs@flink.to>
2021-04-21 22:40:49 +0300

    Add delete button

--- src/components/Notes/NoteList.js
+++ src/components/Notes/NoteList.js
@@ -2,16 +2,29 @@ import React from 'react';
 
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const NoteList = props => {
   return(
     <CardDeck>
       {props.notes.map(note => (
-        <Card key={Math.random()} style={{ minWidth: '30rem', maxWidth: '50rem', marginBottom: '10px' }}>
+        <Card 
+          key={note.id} 
+          style={{ minWidth: '30rem', maxWidth: '40rem', marginBottom: '10px' }}
+        >
           <Card.Body>
             <Card.Subtitle className="mb-4 text-muted">{note.category}</Card.Subtitle>
             <Card.Title style={{ fontSize: '24px' }}>{note.title}</Card.Title>
             <Card.Text>{note.body}</Card.Text>
+            <Button 
+              variant="danger" 
+              size="sm" 
+              style={{ float: 'right' }} 
+              className="float-right"
+              onClick={props.onRemoveItem.bind(this, note.id)}
+            >
+              Delete note
+            </Button>
           </Card.Body>
         </Card>
       ))}
